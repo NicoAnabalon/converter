f6dda66284f6e63f0998d91e1b1d514177bd15dc
NicoAnabalon <nicolas.anabalon.a@gmail.com>
2022-12-15 22:29:29 -0300

    Add base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { gzipSync } from "zlib";
+import { useState } from "react";
 
 function App() {
+  const [convertedJSON, setConvertedJSON] = useState<string>("");
+  const [messageCopied, setMessageCopied] = useState<boolean>(false);
+  const [invalidJSON, setInvalidJSON] = useState<boolean>(false);
+
+  const validateJSON = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    try {
+      const json = JSON.parse(e.target.value);
+      setInvalidJSON(false);
+      parseJSON(JSON.stringify(json));
+    } catch (e) {
+      setInvalidJSON(true);
+    }
+  };
+
+  const parseJSON = (stringifiedJSON: string) => {
+    const gzipped = gzipSync(stringifiedJSON);
+    const base64 = gzipped.toString("base64");
+    setConvertedJSON(() => base64);
+  };
+
+  const copyMessage = () => {
+    if (convertedJSON) {
+      navigator.clipboard.writeText(convertedJSON);
+      setMessageCopied(() => true);
+      setTimeout(() => {
+        setMessageCopied(() => false);
+      }, 3000);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>JSON to GZIP to Base64 Converter</h1>
       </header>
+      <div className="textarea-container">
+        {invalidJSON ? <div className="snackbar error">JSON invalido</div> : ""}
+        <textarea
+          placeholder="Insert JSON here..."
+          onChange={validateJSON}
+        ></textarea>
+      </div>
+      <div className="textarea-container">
+        <textarea
+          placeholder="Waiting for JSON..."
+          value={convertedJSON}
+          readOnly
+          onClick={copyMessage}
+        ></textarea>
+        {messageCopied ? (
+          <div className="snackbar success">Mensaje copiado</div>
+        ) : null}
+      </div>
     </div>
   );
 }
